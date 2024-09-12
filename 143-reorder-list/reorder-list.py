2d22835a9368a558848434379cf47cd58256f272
hchdhry <110125007+hchdhry@users.noreply.github.com>
2024-09-12 16:05:37 +0100

    Time: 58 ms (43.34%) | Memory: 24.6 MB (88.43%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -1,45 +1,30 @@
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
         if not head or not head.next:
             return
-
-        # Find the middle of the list
-        middle = self.findMiddle(head)
         
-        # Split the list
-        second_half = middle.next
-        middle.next = None
-        
-        # Reverse the second half
-        second_half = self.reverseList(second_half)
-        
-        # Merge the two halves
-        self.mergeLists(head, second_half)
-
-    def findMiddle(self, head):
-        slow = fast = head
-        while fast.next and fast.next.next:
+       
+        slow, fast = head, head.next
+        while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
-        return slow
-
-    def reverseList(self, head):
+   
+        second = slow.next
+        slow.next = None
         prev = None
-        current = head
-        while current:
-            next_temp = current.next
-            current.next = prev
-            prev = current
-            current = next_temp
-        return prev
-
-    def mergeLists(self, l1, l2):
-        while l2:
-            next_l1 = l1.next
-            next_l2 = l2.next
-
-            l1.next = l2
-            l2.next = next_l1
-
-            l1 = next_l1
-            l2 = next_l2
+        
+        while second:
+            tmp = second.next
+            second.next = prev
+            prev = second
+            second = tmp
+      
+        first, second = head, prev
+        while second:
+            tmp1, tmp2 = first.next, second.next
+            first.next = second
+            second.next = tmp1
+            first, second = tmp1, tmp2
