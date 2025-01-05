e1cbf9a7c224717bcb8c833df9860bfd7d48f1b0
hchdhry <110125007+hchdhry@users.noreply.github.com>
2025-01-05 13:26:19 +0000

    Time: 0 ms (100.00%) | Memory: 17.8 MB (18.95%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -7,16 +7,17 @@ class Solution:
             '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl',
             '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'
         }
-
-        combinations = []
-
-        def backtrack(combination, index):
-            if index == len(digits):
-                combinations.append(combination)
+        
+        res = []
+        def backtrack(i,cur):
+            if len(cur) == len(digits):
+                res.append(cur)
                 return
-
-            for letter in keys[digits[index]]:
-                backtrack(combination + letter, index + 1)
-
-        backtrack('', 0)
-        return combinations
+            currentLetter = digits[i]
+            for letter in keys[currentLetter]:
+                cur += letter
+                backtrack(i+1,cur)
+                cur = cur[:-1]
+        backtrack(0,"")
+        return res
+            
