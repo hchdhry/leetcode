0649e8e3461e3dfecb897c225f7c95aa736f1862
hchdhry <110125007+hchdhry@users.noreply.github.com>
2025-01-09 20:15:18 +0000

    Time: 0 ms (100.00%) | Memory: 17.8 MB (24.08%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -1,13 +1,16 @@
 class Solution:
     def isHappy(self, n: int) -> bool:
         def search(cur, seen):
+            sum_of_squares = 0
             if cur == 1:
                 return True
             if cur in seen:
                 return False
             
             seen.add(cur)
-            sum_of_squares = sum(int(num) ** 2 for num in str(cur))
+            for num in str(cur):
+                sum_of_squares += int(num) ** 2
+             
             return search(sum_of_squares, seen)
         
         return search(n, set())
