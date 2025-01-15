0d37a33de3c52580e4d78025084fa9b553bc5bdf
hchdhry <110125007+hchdhry@users.noreply.github.com>
2025-01-15 00:04:23 +0000

    Time: 10 ms (21.16%) | Memory: 19.9 MB (7.59%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -3,7 +3,7 @@ class Solution:
         hashmap = {}
         for num in nums:
             hashmap[num] = hashmap.get(num,0)+1
-        key = filter(lambda x: hashmap[x] == 1,hashmap)
-        return (list(key)[0])
+        key = next((k for k in hashmap if hashmap[k] == 1), None)
+        return key
 
         
